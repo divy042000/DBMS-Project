d01e7a2aca9c1a0c8a8666a474b4381bd96b2180
Patel Divy <divypatel0403@gmail.com>
2023-04-14 16:14:04 +0530

    UPDATE

--- server.js
+++ server.js
@@ -1,28 +1,49 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: 'Divypatel',
+  port: 5432,
+});
+
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-const port=4500
-const query =require('./queries')
-const path =require('path')
-const pool = require('./dbconfig').pool
 
-// Middleware
-app.use(bodyParser.json());
-app.set('view engine','ejs'); 
-app.set('views', path.join(__dirname, 'views'));
-app.engine('ejs', require('ejs').__express);
+// middle ware
+app.set('view engine', 'ejs'); // set EJS as the view engine
+app.use(express.static('public')); // serve static files in the "public" directory
 
 
 
-app.get('/moviedashboard',async(req,res)=>{
-    console.log("Get Dashboard")
-    res.send("This is the movie dashboard");
-})
+app.get('/k', function(req, res) {
+  res.render('home');
+});
 
-app.listen(port,()=>{
-    console.log(`listening on port ${port}`)
-})
-setTimeout(() => {
-    server.close();
-    console.log('Server stopped');
-  }, 5000);
+
+app.get('/users', async (req, res) => {
+  try {
+    console.log('Connected Sucess')
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM movie');
+    // const users = result.rows;
+    console.log(result.rows);
+    const data = {
+      movie_id: result.rows[1].movie_id,
+      movie_name:result.rows[1].movie_name,
+      movie_language: result.rows[1].movie_language,
+      movie_hour: result.rows[1].movie_hour,
+      genre: result.rows[1].genre
+    };
+    res.render(__dirname + '/views/home.ejs', {data});
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
+
+app.listen(5000, () => {
+  console.log('App listening on port 3000!');
+});
